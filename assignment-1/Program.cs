464c3542e72ff4c7014531badf89d778aae89ab8
sun1zu <ghostkpubmail@gmail.com>
2025-03-15 11:37:25 +0300

    migrate to github

---
+++ assignment-1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckEven(9901, 2));
+        }
+
+        static bool CheckEven(int number, int divider)
+        {
+            if (number % divider == 0) return true;
+            else if (number % divider != 0 && divider >= 2 && divider <= Math.Sqrt(number)) { 
+                return CheckEven(number, divider+1); 
+            }
+            else return false;
+        }
+    }
+}
