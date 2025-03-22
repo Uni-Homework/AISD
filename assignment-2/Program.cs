c5ea5cd201584f01c043dd5d1bc28050cad58a41
sun1zu <ghostkpubmail@gmail.com>
2025-03-22 16:05:25 +0300

    add oddevensort, WIP as-2

---
+++ assignment-2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+
+namespace assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for(int i=0; i<n; i++) arr[i] = Convert.ToInt32(Console.ReadLine());   
+
+            arr = OddEvenSort(arr);
+            foreach (int i in arr)
+            {
+                Console.Write("" + i + " ");
+            }
+            Console.WriteLine();
+        }
+        
+        static int[] OddEvenSort(int[] nums)
+        {
+            bool isSorted = false;
+            while(!isSorted){
+                isSorted = true;
+                for (int i=0; i<nums.Length-1; i += 2)
+                {
+                    if (nums[i] > nums[i + 1])
+                    {
+                        int tmp = nums[i];
+                        nums[i] = nums[i + 1];
+                        nums[i + 1] = tmp;
+                        isSorted = false;
+                    }
+                }
+                for (int i = 1; i < nums.Length - 1; i += 2)
+                {
+                    if (nums[i] > nums[i + 1])
+                    {
+                        int tmp = nums[i];
+                        nums[i] = nums[i + 1];
+                        nums[i + 1] = tmp;
+                        isSorted = false;
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+}
