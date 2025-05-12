1170468c07a480d15e202c1cf442e032aabe73e5
sun1zu <kiri.andreev2006@yandex.ru>
2025-05-12 20:59:25 +0300

    add + done lab6

---
+++ lab_06/Deque.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+
+namespace lab_06;
+
+public class DoublyNode<T>
+{
+    public DoublyNode(T data)
+    {
+        Data = data;
+    }
+    public T Data { get; set; }
+    public DoublyNode<T> Previous { get; set; }
+    public DoublyNode<T> Next { get; set; }
+}
+
+
+public class Deque<T> : IEnumerable<T>
+{
+    private DoublyNode<T> _head;
+    private DoublyNode<T> _tail;
+    public int Count { get; private set; } = 0;
+
+    public void AddLast(T data)
+    {
+        DoublyNode<T> newNode = new DoublyNode<T>(data);
+        
+        if(_head == null) _head = newNode;
+        else
+        {
+            _tail.Next = newNode;
+            newNode.Previous = _tail;
+        }
+
+        _tail = newNode;
+        Count++;
+    }
+
+    // test
+    public void AddFirst(T data)
+    {
+        DoublyNode<T> node = new DoublyNode<T>(data);
+        
+        if(_head == null) _head = node;
+        else
+        {
+            _head.Next = node;
+            node.Previous = _head;
+        }
+        _head = node;
+        Count++;
+        
+    }
+
+    // public void InsertAfter(DoublyNode<T> node, T data)
+    // {
+    //     if (node == null) throw new ArgumentNullException();
+    //     DoublyNode<T> newNode = new DoublyNode<T>(data);
+    //     
+    //     node.Next = newNode;
+    //     newNode.Previous = node;
+    //     
+    //     if(node.Next != null)
+    //     {
+    //         var nextNode = node.Next;
+    //         newNode.Next = nextNode;
+    //         nextNode.Previous = newNode;
+    //     }
+    //
+    //     Count++;
+    // }
+
+    public T RemoveFirst()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException();
+        T output = _head.Data;
+        if(Count==1)
+        {
+            _head = _tail = null;
+        }
+        else
+        {
+            _head = _head.Next;
+            _head.Previous = null;
+        }
+        Count--;
+        return output;
+    }
+    
+    public T RemoveLast()
+    {
+        if (Count == 0) throw new InvalidOperationException();
+        T output = _tail.Data;
+        if (Count == 1)
+        {
+            _head = _tail = null;
+        }
+        else
+        {
+            _tail = _tail.Previous;
+            _tail.Next = null;
+        }
+        Count--;
+        return output;
+    }
+    
+    public T First
+    {
+        get
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+            return _head.Data;
+        }
+    }
+    public T Last
+    {
+        get
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+            return _tail.Data;
+        }
+    }
+
+    public bool IsEmpty { get { return Count == 0; } }
+
+    public void Clear()
+    {
+        _head = null;
+        _tail = null;
+        Count = 0;
+    }
+
+    public bool Contains(T data)
+    {
+        DoublyNode<T> current = _head;
+        while (current != null)
+        {
+            if (current.Data.Equals(data))
+                return true;
+            current = current.Next;
+        }
+        return false;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)this).GetEnumerator();
+    }
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        DoublyNode<T> current = _head;
+        while (current != null)
+        {
+            yield return current.Data;
+            current = current.Next;
+        }
+    }
+}
---
+++ lab_06/Program.cs
@@ -0,0 +1,89 @@
+﻿using lab_06;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // создать объект пользовательского класса
+        ConsoleKeyInfo K;
+
+        Deque<int> deque = new Deque<int>();
+
+        // add random instance
+        Random r = new Random();
+        do
+        {
+            // 15, 30
+            Console.Clear(); //очистка экрана перед выводом меню
+            Console.WriteLine("1. Create deque");
+            Console.WriteLine("2. Task 18");
+            Console.WriteLine("3. Task 22");
+            Console.WriteLine("4. Out the deque");
+            K = Console.ReadKey(); //считывание кода вводимой клавиши
+            switch (K.Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1: // если нажата клавиша с цифрой 1
+                {
+                    // действие
+                    deque.Clear();
+                    for (int i = 0; i < 10; i++) deque.AddLast(r.Next(1, 20));
+                    OutDeque(deque);
+                    break;
+                }
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2: // если нажата клавиша с цифрой 2
+                {
+                    // действие
+                    // 18
+                    OutDeque(deque);
+                    int sum = 0;
+                    foreach (int i in deque) sum += i;
+
+                    Deque<int> newDeque = new Deque<int>();
+                    while (deque.Count > 0)
+                    {
+                        var element = deque.RemoveFirst();
+                        newDeque.AddLast(element);
+                        newDeque.AddLast(sum);
+                    }
+                    OutDeque(newDeque);
+                    
+                    break;
+                }
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3: // если нажата клавиша с цифрой 3
+                {
+                    OutDeque(deque);
+                    
+                    Deque<int> newDeque = new Deque<int>();
+                    while (deque.Count > 0)
+                    {
+                        var element = deque.RemoveLast();
+                        newDeque.AddLast(element);
+                    }
+                    
+                    OutDeque(newDeque);
+                        
+                    break;
+                }
+                case ConsoleKey.D4:
+                case ConsoleKey.NumPad4: // если нажата клавиша с цифрой 4
+                {
+                    // действие
+                    OutDeque(deque);
+                    break;
+                }
+            }
+            // Приостанавливаем выполнение текущего потока на заданное число времени. Время измеряется в миллисекундах
+            Thread.Sleep(2000); // 2 сек.
+        } while (K.Key != ConsoleKey.Escape); // цикл заканчивается, если нажата клавиша Esc
+    }
+
+
+    static void OutDeque(Deque<int> deque)
+    {
+        Console.WriteLine();
+        foreach (var item in deque) Console.Write($"{item} ");
+    }
+}
