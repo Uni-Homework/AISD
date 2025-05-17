7c14dcb5f5e73f899313df1e06ad95e0508f55ae
sun1zu <kiri.andreev2006@yandex.ru>
2025-05-17 06:39:03 +0300

    done lab_11

--- lab_11/Program.cs
+++ lab_11/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿class Program
+{
+    static void Main()
+    {
+        string file1 = "price1.txt";
+        string file2 = "price2.txt";
+        if (!File.Exists("price1.txt")) File.Create("price1.txt");
+        if (!File.Exists("price2.txt")) File.Create("price2.txt");
+
+        Dictionary<string, int> finalPriceList = new Dictionary<string, int>();
+        
+        void ProcessFile(string filePath)
+        {
+            foreach (string line in File.ReadLines(filePath))
+            {
+                string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 2 && int.TryParse(parts[1], out int price))
+                {
+                    string product = parts[0];
+
+       
+                    if (finalPriceList.ContainsKey(product))
+                    {
+                        finalPriceList[product] = Math.Max(finalPriceList[product], price);
+                    }
+                    else
+                    {
+                        finalPriceList[product] = price;
+                    }
+                }
+            }
+        }
+
+        ProcessFile(file1);
+        ProcessFile(file2);
+     
+        foreach (var item in finalPriceList)
+        {
+            Console.WriteLine($"{item.Key} {item.Value}");
+        }
+    }
+}
